c3039725601c2bea24bfd3f535d08e55a199adb8
lejdhbdjd <108966744+lejdhbdjd@users.noreply.github.com>
2022-08-20 15:06:23 +0900

    Create 25304.c

---
+++ 25304.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int m; scanf("%d", &m);
+    int cnt; scanf("%d", &cnt);
+
+    int sum = 0;
+    while(cnt--){
+        int a, b; scanf("%d %d", &a, &b);
+        sum += a * b;
+    }
+    
+    if ( sum == m ) printf("Yes");
+    else printf("No");
+    
+    return 0;
+}
