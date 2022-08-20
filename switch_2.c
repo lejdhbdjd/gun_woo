9288eda8f19e2866ecedf588e0bfc2588a105bcc
lejdhbdjd <108966744+lejdhbdjd@users.noreply.github.com>
2022-08-20 14:36:23 +0900

    Create switch_2.c

---
+++ switch_2.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main(void) {
+  int n;
+  scanf("%d",&n);
+  switch (n)
+    {
+      case 1 :;
+      case 8 :;
+      case 15 :;
+      case 22 :;
+      case 29 :;
+      printf("토");
+      break;
+      case 2 :;
+      case 9 :;
+      case 16 :;
+      case 23 :;
+      case 30 :;
+      printf("일");
+      break;
+      case 3 :;
+      case 10 :;
+      case 17 :;
+      case 24 :;
+      break;
+      printf("월");
+      case 4 :;
+      case 11 :;
+      case 18 :;
+      case 25 :;
+      break;
+      printf("화");
+      case 5 :;
+      case 12 :;
+      case 19 :;
+      case 26 :;
+      break;
+      printf("수");
+      case 6 :;
+      case 13 :;
+      case 20 :;
+      case 27 :;
+      break;
+      printf("목");
+      case 7 :;
+      case 14 :;
+      case 21 :;
+      case 28 :;
+      break;
+      printf("금");
+      
+    }
+  return 0;
+}
