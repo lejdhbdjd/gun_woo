8c0ddc48d2162e325a689f99c5b6a3155535596a
lejdhbdjd <108966744+lejdhbdjd@users.noreply.github.com>
2022-10-15 15:00:37 +0900

    Create 2022.10.15.c

---
+++ 2022.10.15.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+int main(void) 
+  int ar2[3][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+  int *p;
+  int i;
+  int len = sizeof(ar[0])/sizeof(ar[0][0]);
+
+  for (i = 0; i <1en;i++)
+  {
+    printf("%d",*(ar[1]+i));      
+  }
+  printf("\n")
+
+  p = ar[2];
+
+  for (i = 0; i <1en;i++)
+  {
+    printf("%d",*(p+i));      
+  }
+  printf("\n")
+}
